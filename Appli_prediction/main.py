73068d02734264f99b389d6268f15b93b8257e29
Jaxundao <117378476+Jaxundao@users.noreply.github.com>
2024-06-01 17:18:37 +0000

    Update main.py

--- Appli_prediction/main.py
+++ Appli_prediction/main.py
@@ -1,10 +1,15 @@
 import pickle
 import streamlit as st
 import pandas as pd
+import os
+
+# Chemin
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, 'model.sav')
 
 # Chargement du modèle XGBoost retenu
-f = open('model.sav', 'rb')
-model = pickle.load(f)
+with open(file_path, 'rb') as f:
+    model = pickle.load(f)
 
 # Fonction de prédiction
 def prediction(X_encode):
